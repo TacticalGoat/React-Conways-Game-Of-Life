4cf32c9b61873d55d8a1f2c38f31ecd8ecd4250d
Arjun Narayan <arjun@hubert.org>
2018-05-15 22:45:56 +0530

    Added canvas

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Stage, Layer, Rect, Text } from 'react-konva';
+import Konva from 'konva';
 import './index.css';
 
-function Cell(props) {
+/*function Cell(props) {
     return(
         <button className = {props.className}  onClick={props.onClick}/>
     );
+}*/
+
+function cell(props) {
+    const {ctx, x, y, width, height, alive} = props;
+    console.log(`Drawing ${x},${y}`);
+    if(alive === true)
+        ctx.fillRect(x, y, width, height);
+    ctx.rect(x, y, width, height);
 }
 
-class Board extends React.Component {
+/*class Board extends React.Component {
     render(){
         let rows = Array(parseInt(this.props.rows)).fill(true);
         let cols = Array(parseInt(this.props.cols)).fill(true);
@@ -31,6 +41,55 @@ class Board extends React.Component {
                     })
                 }
             </ul>
+        );
+    }
+}*/
+
+class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            height: this.props.rows * 12,
+            width: this.props.cols * 12,
+            board: this.props.board
+        }
+
+    }
+    drawCanvas() {
+        const canvas = this.refs.canvas;
+        const ctx = canvas.getContext("2d");
+        ctx.clearRect(0,0,this.state.width,this.state.height);
+        ctx.lineWidth = 2;
+        ctx.lineCap = 'square';
+        ctx.strokeStyle = '#c0c0c0';
+        ctx.fillStyle = '#000';
+        for(let row = 0; row < this.props.rows; row++) {
+            for(let col = 0; col < this.props.cols; col++) {
+                let y = row * 12 + 2;
+                let x = col * 12 + 2;
+                let alive = this.state.board[row][col];
+                ctx.beginPath();
+                ctx.rect(x, y, 9, 9);
+                if(alive === true)
+                    ctx.fill();
+                ctx.stroke();
+            }
+        }
+    }
+
+
+
+    componentDidMount() {
+        this.drawCanvas();
+    }
+
+    componentDidUpdate() {
+        this.drawCanvas();
+    }
+
+    render() {
+        return(
+            <canvas ref="canvas" width={this.state.width} height={this.state.height} onClick={this.props.onClick} />
         );
     }
 }
@@ -80,7 +139,7 @@ class Game extends React.Component {
             return false;
         if(noAlive === 3 && !cellAlive)
             return true
-       
+        //console.log(`${row},${col} Alive: ${cellAlive}`)
         return cellAlive
     }
 
@@ -100,18 +159,18 @@ class Game extends React.Component {
             {
                 board: nState,
             }
-        )
+        );
     }
 
     start(){
-        this.intervalID = setInterval(() => this.getNextState(), 150);
+        this.intervalID = setInterval(() => this.getNextState(), 1000);
     }
 
     stop(){
         clearInterval(this.intervalID);
     }
 
-    handleClick(row, col) {
+    /*handleClick(row, col) {
         row = parseInt(row)
         col = parseInt(col)
         const board = this.state.board.slice();
@@ -120,19 +179,38 @@ class Game extends React.Component {
         this.setState({
             board: board,
         });
+    }*/
+
+    handleClick(e) {
+        console.log("CLICK");
+        const rect = ReactDOM.findDOMNode(this.refs.board).getBoundingClientRect();
+        let x = e.clientX - rect.left;
+        let y = e.clientY - rect.top;
+        let row = Math.floor(y / 12);
+        let col = Math.floor(x / 12);
+        const board = this.state.board.slice();
+        board[row][col] = !board[row][col];
+        this.setState({
+            board: board,
+        });
     }
+    
 
     render(){
-        return(<div>
-               <Board board={this.state.board} rows={this.props.rows} cols={this.props.cols} onClick={(row,col) => this.handleClick(row, col)}/>
-               <button onClick={() => this.getNextState()}> Next </button>
-               <button onClick={() => this.start()}> Start </button>
-               <button onClick={() => this.stop()}> Stop </button>
-               </div>);
+        return(
+        <div>
+            <div>
+                <Board ref="board" board={this.state.board} rows={this.props.rows} cols={this.props.cols} onClick = {this.handleClick.bind(this)}/>
+            </div>
+            <button onClick={() => this.getNextState()}> Next </button>
+            <button onClick={() => this.start()}> Start </button>
+            <button onClick={() => this.stop()}> Stop </button>
+        </div>
+        );
     }
 }
 
 ReactDOM.render(
-    <Game rows="30" cols="60" />,
+    <Game rows="5" cols="5" />,
     document.getElementById('root')
 );
